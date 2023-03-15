672f6c738d55c30cf189d6b47240d82761507b20
shyamolina <ghoshshyamolina89@gmail.com>
2023-03-15 13:32:26 +0100

    climate data

---
+++ DATA/CHELSA_v2/README.txt
@@ -0,0 +1,6 @@
+CHELSA data (version 2.1,https://chelsa-climate.org/timeseries/) folder, 
+see detailed path here https://envicloud.wsl.ch/#/?prefix=chelsa%2Fchelsa_V2%2FGLOBAL%2Fmonthly%2F
+
+Global raw monthly timeseries data for pr (1979-2018) and tas, tasmin, tasmax (1979-2019) were downloaded using script 
+in R folder: R/download_rawdata_CHELSA.R
+
---
+++ DATA/CHELSA_v2/monthly/tasmin/extract_data_for_uRID_WP3.R
@@ -0,0 +1,33 @@
+###############################################################################################################
+# This file has to be in the same folder where you save the environmental data 
+# that you will extract
+###############################################################################################################
+rm(list=ls())
+path<-dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(path)
+library(raster)
+library(sp)
+rastlist <- list.files(path = "./", pattern='.tif$', all.files=TRUE, full.names=FALSE)
+id<-as.integer(substr(rastlist,18,21))
+idsel<-which(id%in%c(1996:2019))
+rastlist<-rastlist[idsel]
+
+allrasters <- raster::stack(rastlist)
+df_lonlat<-read.csv("../uRID_lonlat_stratum.csv")
+
+df_lonlat_table<-df_lonlat
+
+# now make it as sp object
+coordinates(df_lonlat) <- ~Longitude + Latitude
+proj4string(df_lonlat) <- sp::CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+class(df_lonlat)
+
+get_values<- raster::extract(allrasters, df_lonlat)
+df_env<- as.data.frame(get_values)
+cnm<-colnames(df_env)
+cnm<-substr(cnm,8,21)
+colnames(df_env)<-cnm # month_year
+df_env <- cbind(df_lonlat_table,df_env)
+write.csv(df_env,"../tasmin_monthlyvalues_extracted_for_uRID_WP3.csv",row.names=F)
+
+
