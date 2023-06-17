2b0e67ce6459f61355095476e225fc99098e2a0b
shyamolina <ghoshshyamolina89@gmail.com>
2023-06-17 15:43:41 +0200

    edit

--- R/Master.R
+++ R/Master.R
@@ -3,6 +3,7 @@
 #setwd(path)
 #--------------------
 library(here)
+source(here("R/method_fig.R")) # conceptual figure introducing tail-dependence
 source(here("R/get_birds_data.R")) # wrangle raw data
 source(here("R/prepare_abund_data.R")) # for a given species, get abundance data in required format
 source(here("R/call_spat_syn_for_abund.R"))# call for all species' abundance data to compute spat syn
